62dea7003e0903be999db86192068df7e00af6f7
voncho <fencho.jay@gmail.com>
2023-08-03 15:33:10 +0800

    ci: 后端调试(登录 用户信息)

--- src/service/request/index.ts
+++ src/service/request/index.ts
@@ -6,5 +6,9 @@ const { url, proxyPattern } = getServiceEnvConfig(import.meta.env);
 const isHttpProxy = import.meta.env.VITE_HTTP_PROXY === 'Y';
 
 export const request = createRequest({ baseURL: isHttpProxy ? proxyPattern : url });
+export const requestX = createRequest({
+  baseURL: isHttpProxy ? proxyPattern : url,
+  headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+});
 
 export const mockRequest = createRequest({ baseURL: '/mock' });
